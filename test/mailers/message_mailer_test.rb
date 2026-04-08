d5aa46e6cf58620324aac169dd7d71ddbfeb0827
REDIOUS <mverhaeghe5@gmail.com>
2026-04-08 20:26:50 +0200

    feat: style contact acknowledgement email

--- test/mailers/message_mailer_test.rb
+++ test/mailers/message_mailer_test.rb
@@ -21,8 +21,12 @@ class MessageMailerTest < ActionMailer::TestCase
   test "acknowledgement builds a visitor confirmation email" do
     original_admin_email = ENV["ADMIN_EMAIL"]
     original_mailer_from = ENV["MAILER_FROM"]
+    original_app_host = ENV["APP_HOST"]
+    original_force_ssl = ENV["FORCE_SSL"]
     ENV["ADMIN_EMAIL"] = "admin@example.com"
     ENV["MAILER_FROM"] = "no-reply@redious.fr"
+    ENV["APP_HOST"] = "kois-story.test"
+    ENV["FORCE_SSL"] = "true"
 
     mail = MessageMailer.acknowledgement(messages(:one))
 
@@ -31,8 +35,12 @@ class MessageMailerTest < ActionMailer::TestCase
     assert_equal [ "admin@example.com" ], mail.reply_to
     assert_match "bien été reçu", mail.subject
     assert_match "spams", mail.text_part.body.to_s
+    assert_includes mail.html_part.body.to_s, "Koi's Story"
+    assert_includes mail.html_part.body.to_s, "https://kois-story.test/docs/assets/LOGO%20MANU%20FINI.png"
   ensure
     ENV["ADMIN_EMAIL"] = original_admin_email
     ENV["MAILER_FROM"] = original_mailer_from
+    ENV["APP_HOST"] = original_app_host
+    ENV["FORCE_SSL"] = original_force_ssl
   end
 end
